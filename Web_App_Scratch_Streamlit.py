6d92b8ef247678eb8ec5eb25a95ca15db0d50cbf
Tera <66988502+TERAKOSHITAISUKE@users.noreply.github.com>
2021-12-11 21:31:26 +0900

    Update Web_App_Scratch_Streamlit.py

--- Web_App_Scratch_Streamlit.py
+++ Web_App_Scratch_Streamlit.py
@@ -100,7 +100,7 @@ def main():
             		col1, col2 = st.columns(2)
 
             		with col1:
-            			with st.beta_expander("Basic Info"):
+            			with st.st.expander("Basic Info"):
             				st.success("Text Stats")
             				word_desc = nt.TextFrame(raw_text).word_stats()
             				result_desc = {"Length of Text":word_desc['Length of Text'],
@@ -109,18 +109,18 @@ def main():
 											"Num of Stopwords":word_desc['Num of Stopwords']}
             				st.write(result_desc)
 
-            			with st.beta_expander("Stopwords"):
+            			with st.st.expander("Stopwords"):
             				st.success("Stop Words List")
             				stop_w = nt.TextExtractor(raw_text).extract_stopwords()
             				st.error(stop_w)
 
             		with col2:
-            			with st.beta_expander("Processed Text"):
+            			with st.st.expander("Processed Text"):
             				st.success("Stopwords Excluded Text")
             				processed_text = str(nt.TextFrame(raw_text).remove_stopwords())
             				st.write(processed_text)
 
-            			with st.beta_expander("Plot Wordcloud"):
+            			with st.st.expander("Plot Wordcloud"):
             			    st.success("Wordcloud")
             			    plot_wordcloud(raw_text)
 
@@ -132,7 +132,7 @@ def main():
             		col3, col4 = st.beta_columns(2)
 
             		with col3:
-            			with st.beta_expander("Tokens&Lemmas"):
+            			with st.st.expander("Tokens&Lemmas"):
             				st.write("T&L")
             				processed_text_mid = str(nt.TextFrame(raw_text).remove_stopwords())
             				processed_text_mid = str(nt.TextFrame(processed_text_mid).remove_puncts())
@@ -141,7 +141,7 @@ def main():
             				st.json(tandl)
 
             		with col4:
-            			with st.beta_expander("Summarize"):
+            			with st.st.expander("Summarize"):
             				st.success("Summarize")
             				summary_text = summarize(raw_text,ratio=0.4)
             				if summary_text != "":
